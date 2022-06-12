0eb5e04b2adcc348de6092d665a7f90e514f0202
Alex Howes <howesalexandra@gmail.com>
2022-06-12 11:37:11 +0200

    Remove the old pull domiporta listings script and instead create classes: a general class for pulling listings from any listing site, and a child class to pull listings from the domiporta site.

---
+++ data/domiporta/pull_domiporta_listings.py
@@ -0,0 +1,175 @@
+import sys
+import yaml
+sys.path.append('..')
+from pull_listings import PullPropertyListings
+
+
+class PullDomiportaListings(PullPropertyListings):
+    """
+    Pull property listings from the Polish real estate listing site, Domiporta.
+    """
+    def __init__(self):
+        """
+        """
+        root_url = 'https://www.domiporta.pl'
+        page_param = 'PageNumber'
+        super().__init__(root_url, page_param)
+
+
+    def get_listings_list(self, listings_page_soup):
+        """
+        Get the list of listings from the listings page.
+
+        Parameters
+        ----------
+        listings_page_soup : BeautifulSoup (required)
+            Soup of the listings page.
+
+        Returns
+        -------
+        listings_list : list
+            List of listings.
+        """
+        listings_list = listings = listings_page_soup.find("div", {"class": "listing"})\
+                                                     .find("div", {"class": "listing__container"})\
+                                                     .find("ul", {"class": "grid"})\
+                                                     .find_all("li", {"class": "grid-item"})
+        return listings_list
+
+
+    def get_listing_url(self, listing_soup):
+        """
+        Get the relative url of a single listing from the single listing soup on the listings page.
+
+        Parameters
+        ----------
+        listing_soup : BeautifulSoup (required)
+            Soup of the listing from the listings list.
+
+        Returns
+        -------
+        url : string
+            Relative URL of the single listing.
+        """
+        sneakpeek_info = listing_soup.find("div", {"class": "sneakpeak__data"})
+        if not sneakpeek_info:
+            return
+        url = sneakpeek_info.find("a", {"class": "sneakpeak__title"})['href']
+        return url
+
+
+    def get_listing_details(self, listing_page_soup, listing_details_translations=None):
+        """
+        Get details of the property listing from the listing soup.
+
+        Parameters
+        ----------
+        listing_page_soup : BeautifulSoup (required)
+            Soup of the listing page.
+
+        listing_details_translations : dict (default=None)
+            Dictionary mapping listing detail names to a translation.
+
+        Returns
+        -------
+        listing_details : dict
+            Dictionary mapping listing detail names to content, e.g. {"title": "My listing", "category": "flat"}.
+        """
+        listing_details = {}
+        header_section = listing_page_soup.find("div", {"class": "detials__header-section"})
+        listing_details["title"] = header_section.find("span", {"class": "summary__subtitle-2"}).text.strip()
+
+        # Get the price and currency
+        price_info = header_section.find("span", {"class": "summary__price_number"})
+        price_number = price_info.find("span", {"itemprop": "price"})
+        if price_number:
+            listing_details["price"] = price_number["content"]
+            listing_details["currency"] = price_info.find("span", {"itemprop": "priceCurrency"})["content"]
+        else:
+            listing_details["price"] = price_info.text.strip()
+
+        # Extract information from the details section
+        details_section = listing_page_soup.find("section", {"class": ["detials__section", "features"]}).find("div", {"class": "features__container"}).find("ul", {"class": "features__list-2"}).find_all("li")
+        for item in details_section:
+            detail_title = item.find("span", {"class": "features__item_name"})
+            if not detail_title: continue
+            detail_title = detail_title.text.strip()
+            detail_value = item.find("span", {"class": "features__item_value"})
+
+            # Check if the information is known
+            if detail_title not in listing_details_translations:
+                print(f'Unrecognised listing detail: {detail_title.strip()}')
+                continue
+
+            # Extract the information depending on which item
+            detail_title_trans = listing_details_translations[detail_title]
+            if detail_title=='Lokalizacja':
+                address_details = detail_value.find("span", {"itemprop": "address"})
+                listing_details["address_locality"] = address_details.find("span", {"itemprop": "addressLocality"}).text.strip()
+                street_address = address_details.find("span", {"itemprop": "streetAddress"})
+                if street_address:
+                    listing_details["street_address"] = street_address.text.strip()
+                region_info = address_details.find("meta", {"itemprop": "addressRegion"})
+                if region_info:
+                    listing_details["region"] = region_info["content"].strip()
+                country_info = address_details.find("meta", {"itemprop": "addressCountry"})
+                if country_info:
+                    listing_details["country"] = country_info["content"].strip()
+                postcode_info = address_details.find("meta", {"itemprop": "postalCode"})
+                if postcode_info:
+                    listing_details["postcode"] = postcode_info["content"].strip()
+                geo = detail_value.find("span", {"itemprop": "geo"})
+                if geo:
+                    listing_details["latitude"] = geo.find("meta", {"itemprop": "latitude"})["content"].strip()
+                    listing_details["longitude"] = geo.find("meta", {"itemprop": "longitude"})["content"].strip()
+            elif detail_title in ['Kategoria', 'Przeznaczenie']:
+                listing_details[detail_title_trans] = detail_value.find("a").text.strip()
+            else:
+                listing_details[detail_title_trans] = detail_value.text.strip()
+
+        return listing_details
+
+
+    def process_listing_data(self, listings_data, listing_details_names):
+        """
+        Process the dataset of listings.
+
+        Parameters
+        ----------
+        listings_data : Pandas DataFrame (required)
+            Pandas DataFrame with one row per listing.
+        """
+        # Process the data to convert numbers with units to just numbers
+        def convert_units_to_num(price, unit=None):
+            try:
+                price_num = float(''.join(price.strip().split(unit)[0].replace(',','.').split()))
+                return price_num
+            except:
+                return
+        listings_data['price_num'] = listings_data['price'].apply(lambda x: convert_units_to_num(x, unit='zł'))
+        listings_data['price_zl_per_m2_num'] = listings_data['price_zl_per_m2'].apply(lambda x: convert_units_to_num(x, unit='zł/m'))
+        listings_data['surface_area_m2_num'] = listings_data['surface_area_m2'].apply(lambda x: convert_units_to_num(x, unit='m2'))
+        listings_data['latitude'] = listings_data['latitude'].apply(lambda x: convert_units_to_num(x))
+        listings_data['longitude'] = listings_data['longitude'].apply(lambda x: convert_units_to_num(x))
+
+        # Order the columns so that the appending to the data is consistent
+        columns_order = ['title', 'listing_page_category', 'price', 'price_num', 'currency', 'price_zl_per_m2', 'price_zl_per_m2_num', 'surface_area_m2', 'surface_area_m2_num', 'address_locality', 'street_address', 'region', 'country', 'postcode', 'latitude', 'longitude']
+        columns_order += [item for item in listing_details_names if item not in columns_order] + ['url']
+        for column in columns_order:
+            if column not in listings_data.columns:
+                listings_data[column] = ''
+        missed_columns = [column for column in listings_data.columns if column not in columns_order]
+        if missed_columns:
+            raise RuntimeError(f'Missing columns: {missed_columns}')
+        listings_data = listings_data[columns_order]
+
+        return listings_data
+
+
+if __name__ == "__main__":
+    listing_categories = {'mieszkanie/wynajme': 'apartments', 'dom/wynajme': 'houses', 'pokoj/wynajme': 'rooms'}
+    listings_puller = PullDomiportaListings()
+    listings_puller.pull_listing_categories(data_write_path='raw/testing.csv',
+                                            listing_categories=listing_categories,
+                                            listing_details_translations=yaml.safe_load(open('listing_details_translations.yml')),
+                                            page_end=5)
---
+++ data/pull_listings.py
@@ -0,0 +1,135 @@
+"""
+Script to pull rental listings for the Polish estate agent website, Domiporta (https://www.domiporta.pl/).
+
+Note that data will be appended to the file in the data folder, so this file should be deleted if you want to create a new file.
+"""
+import os
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import yaml
+from collections import Counter
+
+class PullPropertyListings:
+    """
+    Class to pull listings from property listing websites and return a dataset.
+
+    Parameters
+    ----------
+    """
+    def __init__(self, root_url, page_param):
+        self.root_url = root_url
+        self.page_param = page_param
+
+
+    def pull_listing_categories(self, listing_categories, data_write_path, listing_details_translations, page_start=1, page_end=None):
+        """
+        Pull listings from multiple listing pages.
+        """
+        # Print a warning if the file already exists
+        if os.path.exists(data_write_path):
+            input(f'WARNING: data will overwrite the existing data file at {data_write_path}. Press enter to continue.')
+            os.remove(data_write_path)
+
+        # Loop through categories, pull data for each category, and save
+        for category_url, category_name in listing_categories.items():
+            self.pull_listings(data_write_path=data_write_path,
+                               page_slug=category_url,
+                               listing_details_translations=listing_details_translations,
+                               extra_listing_info={'listing_page_category': category_name},
+                               page_start=page_start,
+                               page_end=page_end,
+                               mode='a',
+                               suppress_overwrite_warning=True)
+
+
+    def pull_listings(self, data_write_path, page_slug, listing_details_translations, extra_listing_info=None, page_start=1, page_end=None, mode='a', suppress_overwrite_warning=False):
+        """
+        Pull a dataset of listings from the property website and write to a file.
+        By default results are appended to the file to enable stopping and starting.
+
+        Parameters
+        ----------
+        data_write_path : string (required)
+            The location of the CSV file to write the dataset of listings to.
+
+        page_slug : string (required)
+            A slug to be added to the root url to give the URL of the listings page.
+
+        extra_listing_info : dict (default=None)
+            Any extra information to add to the listings dataset, e.g. category information.
+
+        page_start : int (default=1)
+            Number of the page to start, starting from 1.
+
+        page_end : int (default=None)
+            Number of pages to pull. If None, all pages will be pulled.
+
+        listing_details_translations : dict (required)
+            Dictionary mapping listing detail names to a translation. The order of keys will also be used to order the columns of the final dataset.
+
+        mode : string (default='a')
+            Mode used to write the final dataset. Default is append.
+        """
+        # Check if the write file exists and print a warning
+        if not suppress_overwrite_warning:
+            if os.path.exists(data_write_path):
+                if mode=='a':
+                    input(f'WARNING: data will append to the existing data file at {data_write_path}. Press enter to continue.')
+                elif mode=='w':
+                    input(f'WARNING: data will overwrite the existing data file at {data_write_path}. Press enter to continue.')
+        if listing_details_translations is None:
+            listing_details_translations = []
+
+        # Loop through listing pages and pull data
+        page_number = page_start
+        while True:
+            data = pd.DataFrame()
+            print(f'\nSearching listings in page {page_number}... {self.root_url}/{page_slug}?{self.page_param}={page_number}')
+            listings_page = requests.get(f'{self.root_url}/{page_slug}?{self.page_param}={page_number}')
+            listings_soup = BeautifulSoup(listings_page.content, "html.parser")
+
+            # Get the listings and check if redirect or empty page
+            listings = self.get_listings_list(listings_soup)
+            if not listings:
+                break
+            if listings_page.history:
+                if listings_page.history[0].status_code==301:
+                    break
+
+            # Loop through all listings on the page
+            for single_listing in listings:
+                single_listing_url = self.get_listing_url(single_listing)
+                if not single_listing_url:
+                    continue
+                single_listing_url = f'{self.root_url}{single_listing_url}'
+                single_page = requests.get(f'{single_listing_url}')
+                single_soup = BeautifulSoup(single_page.content, "html.parser")
+
+                # Extract individual listing information
+                try:
+                    single_listing_info = self.get_listing_details(listing_page_soup=single_soup,
+                                                                   listing_details_translations=listing_details_translations)
+                except Exception as err:
+                    print(f'Error with listing {single_listing_url}')
+                    raise RuntimeError(err)
+                single_listing_info['url'] = single_listing_url
+
+                # Append the data to the dataframe
+                data = data.append(single_listing_info, ignore_index=True)
+
+            # Process athe data
+            data = self.process_listing_data(listings_data=data,
+                                             listing_details_names=listing_details_translations.values())
+            if extra_listing_info is not None:
+                for name, value in extra_listing_info.items():
+                    data[name] = value
+
+            # Save the dataset
+            print('Saving data...')
+            header = (not os.path.exists(data_write_path)) if mode=='a' else True
+            data.to_csv(data_write_path, index=False, header=header, mode=mode)
+
+            if page_number >= page_end:
+                break
+            page_number += 1
